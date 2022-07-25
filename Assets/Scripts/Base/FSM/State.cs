2335830e20097e2a12c55caecded2140dea811af
Ashechol <34626395+Ashechol@users.noreply.github.com>
2022-07-25 17:51:04 +0800

    添加了状态机基类 (#4)
    * update .idea settings
    
    * 添加状态机基类

---
+++ Assets/Scripts/Base/FSM/State.cs
@@ -0,0 +1,17 @@
+namespace Base.FSM
+{
+    public class State
+    {
+        protected StateMachine stateMachine;
+        
+        public State(StateMachine stateMachine)
+        {
+            this.stateMachine = stateMachine;
+        }
+    
+        public virtual void Enter() {}
+        public virtual void LogicUpdate() {}
+        public virtual void PhysicsUpdate() {}
+        public virtual void Exit() {}
+    }
+}
