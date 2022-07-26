95c8f49a17c219dd4c6507d4299f9ab1780374d7
Ashechol <34626395+Ashechol@users.noreply.github.com>
2022-07-26 11:47:46 +0800

    玩家移动 (#12)
    * update .idea settings
    
    * 添加状态机基类
    
    * merge dev into feature (#5)
    
    * 修改文件夹结构
    
    * IntegerateWwise
    
    * 添加soundbank
    
    * 添加了状态机基类 (#4)
    
    * update .idea settings
    
    * 添加状态机基类
    
    Co-authored-by: ThouJah <makipromise@foxmail.com>
    
    * wwise meta
    
    * 添加Input System
    
    * 添加Input System (#6)
    
    * wwise meta
    
    * 添加Input System
    
    * update gitignore
    
    * update gitignore (#9)
    
    * wwise meta
    
    * 添加Input System
    
    * update gitignore
    
    * remove useless zip
    
    * 新增状态机和Core.Movement
    
    Co-authored-by: ThouJah <makipromise@foxmail.com>

---
+++ Assets/Scripts/Player/PlayerStates/PlayerMoveState.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerMoveState: PlayerState
+    {
+        public PlayerMoveState(Player player, string name) : base(player, name) { }
+
+        public override void PhysicsUpdate()
+        {
+            base.PhysicsUpdate();
+            
+            core.Movement.SetVelocity(InputVec2 * data.moveVelocity);
+        }
+
+        public override void LogicUpdate()
+        {
+            base.LogicUpdate();
+
+            if (InputVec2 == Vector2.zero)
+                StateMachine.ChangeState(player.IdleState);
+        }
+    }
+}
