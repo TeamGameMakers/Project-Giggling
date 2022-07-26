48272aa2d241967ec2af0d0ad19ebdfe6f4132f8
Ashechol <34626395+Ashechol@users.noreply.github.com>
2022-07-26 19:30:58 +0800

    添加了Input Map (#14)
    * update .idea settings
    
    * 添加状态机基类
    
    * merge dev into feature (#5)
    
    * 修改文件夹结构
    
    * IntegerateWwise
    
    * 添加soundbank
    
    * 添加了状态机基类 (#4)
    
    * update .idea settings
    
    * 添加状态机基类
    
    Co-authored-by: ThouJah <makipromise@foxmail.com>
    
    * wwise meta
    
    * 添加Input System
    
    * 添加Input System (#6)
    
    * wwise meta
    
    * 添加Input System
    
    * update gitignore
    
    * update gitignore (#9)
    
    * wwise meta
    
    * 添加Input System
    
    * update gitignore
    
    * remove useless zip
    
    * 新增状态机和Core.Movement
    
    * 添加了新的按键
    
    * 添加了新的按键
    
    Co-authored-by: ThouJah <makipromise@foxmail.com>

--- Assets/Scripts/Input/InputHandler.cs
+++ Assets/Scripts/Input/InputHandler.cs
@@ -6,28 +6,100 @@ public class InputHandler : MonoBehaviour
 {
     private PlayerInput _playerInput;
 
+    private InputActionMap _playerMap;
+    private InputActionMap _cameraMap;
+    private InputActionMap _lockPickMap;
+
     private InputAction _move;
     
+    private InputAction _rotate;
+    
+    // Player
     public Vector2 RawMoveInput { get; private set; }
     public int NormInputX { get; private set; }
     public int NormInputY { get; private set; }
+    
+    // Camera
+    public bool InteractInput { get; private set; }
+    public bool ExitInput { get; private set; }
+    
+    // Lock Pick
+    public bool PryInput { get; private set; }
+    public bool PickInput { get; private set; }
 
     private void Awake()
     {
         _playerInput = GetComponent<PlayerInput>();
-        _move = _playerInput.actions["Move"];
+        _playerMap = _playerInput.actions.FindActionMap("Player", true);
+        _cameraMap = _playerInput.actions.FindActionMap("SurveillanceCam", true);
+        _lockPickMap = _playerInput.actions.FindActionMap("Lock Pick", true);
     }
 
     private void Start()
     {
-        _playerInput.onActionTriggered += OnMoveInput;
+        // Player
+        _playerMap.actionTriggered += OnPlayerMoveInput;
+        _playerMap.actionTriggered += OnCamRotateInput;
+        
+        // Camera
+        _cameraMap.actionTriggered += OnInteractInput;
+        _cameraMap.actionTriggered += OnExitInput;
+
+        // Lock Pick
+        _lockPickMap.actionTriggered += OnPryInput;
+        _lockPickMap.actionTriggered += OnPickInput;
     }
 
-    private void OnMoveInput(InputAction.CallbackContext context)
+    #region Actions
+    
+    // Player
+    private void OnPlayerMoveInput(InputAction.CallbackContext context)
     {
         if (context.action != _move) return;
         RawMoveInput = context.ReadValue<Vector2>();
         NormInputX = Mathf.RoundToInt(RawMoveInput.x);
         NormInputY = Mathf.RoundToInt(RawMoveInput.y);
     }
+    
+    private void OnInteractInput(InputAction.CallbackContext context)
+    {
+        if (context.action.name != "Interact") return;
+
+        InteractInput = context.performed;
+    }
+    
+    // Surveillance Camera
+    private void OnCamRotateInput(InputAction.CallbackContext context)
+    {
+        if (context.action.name != "Rotate") return;
+        NormInputX = Mathf.RoundToInt(context.ReadValue<float>());
+        Debug.Log(NormInputX);
+    }
+
+    private void OnExitInput(InputAction.CallbackContext context)
+    {
+        if (context.action.name != "Exit") return;
+
+        ExitInput = context.performed;
+    }
+
+    private void OnPryInput(InputAction.CallbackContext context)
+    {
+        if (context.action.name != "Pry") return;
+
+        PryInput = context.performed;
+    }
+
+    private void OnPickInput(InputAction.CallbackContext context)
+    {
+        if (context.action.name != "Pick") return;
+
+        PickInput = context.performed;
+    }
+    
+    #endregion
+
+    public void SwitchToPlayer() => _playerInput.SwitchCurrentActionMap("Player");
+    public void SwitchToCamera() => _playerInput.SwitchCurrentActionMap("SurveillanceCam");
+    public void SwitchToLockPick() => _playerInput.SwitchCurrentActionMap("Lock Pick");
 }
---
+++ Assets/Scripts/Interact/InteractTrigger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace Interact
+{
+    [RequireComponent(typeof(Collider2D))]
+    public abstract class InteractTrigger: MonoBehaviour
+    {
+        private Interaction _interaction;
+    }
+}
