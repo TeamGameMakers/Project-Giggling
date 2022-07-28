8e62a5fdf82aa63358149441e0de4e1a2a620884
Arno Clare <ArnoClareF@outlook.com>
2022-07-28 16:57:07 +0800

    修改推针谜题的基础框架。

---
+++ Assets/Scripts/Puzzle/PinLockPuzzleController.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace Puzzle
+{
+    public class PinLockPuzzleController : PuzzleController
+    {
+        private PinLockPuzzleModel m_model;
+        
+        private void Start()
+        {
+            m_model = GetModel<PinLockPuzzleModel>();
+            InputHandler.SwitchToLockPick();
+        }
+
+        private void Update()
+        {
+            switch (m_model.PuzzleState)
+            {
+                case PinLockPuzzleModel.State.Rising:
+                    // 上升时没有操作
+                    break;
+                case PinLockPuzzleModel.State.Falling:
+                    // 下落时检查解锁
+                    if (InputHandler.PickPressed && m_model.TryUnlock())
+                    {
+                        // 解锁成功的逻辑
+                    }
+                    else
+                    {
+                        // 解锁失败的逻辑
+                    }
+                    break;
+                case PinLockPuzzleModel.State.Static:
+                    // 静止时检查推针
+                    if (InputHandler.PryInput)
+                    {
+                        m_model.PushPin();
+                    }
+                    break;
+            }
+        }
+    }
+}
