b0e68e66069b41e62de90427e32bc391475b8569
Arno Clare <31271371+ArnoClare@users.noreply.github.com>
2022-08-03 18:52:17 +0800

    冲突修复 (#46)
    * 完成基础的开始界面、设计界面框架，完成场景切换渐变。
    
    * 实现了基础的剧情对话显示框架。
    
    * 修改剧情对话的框架结构。
    
    * 复原冲突损坏文件
    
    * 修复 Font 冲突问题。
    
    * 修改部分 UI 素材。
    
    * 实现基础的场景触发事件功能。
    
    * 实现了基础的道具交互功能。
    
    * 添加 GameManager.
    
    * 添加基础的 Puzzle 框架。
    
    * 修复 Sprite 图集比例问题，将场景 1 2 墙体设置为 Composite 碰撞体。
    
    * 基本实现推针小游戏功能。
    
    * 合并后小修改。

--- Assets/Scripts/Interact/Interactables/ItemInteractable.cs
+++ Assets/Scripts/Interact/Interactables/ItemInteractable.cs
@@ -1,29 +1,29 @@
-// using Data;
-// using Save;
-// using UI;
-// using UnityEngine;
-//
-// namespace Interact
-// {
-//     public class ItemInteractable : Interactable
-//     {
-//         public ItemDataSO item;
-//         public string SaveKey => "pick_item_" + item.name;
-//         
-//         protected override void Start()
-//         {
-//             base.Start();
-//             // 道具已经被拾取了
-//             if (SaveManager.GetBool(SaveKey))
-//             {
-//                 gameObject.SetActive(false);
-//             }
-//         }
-//
-//         public override void Interact(Interactor interactor)
-//         {
-//             Debug.Log("与道具交互");
-//             UIManager.Instance.ShowPanel<ItemInfoPanel>("ItemInfoPanel", callBack: panel => panel.UpdateInfo(item));
-//         }
-//     }
-// }
+using Data;
+using Save;
+using UI;
+using UnityEngine;
+
+namespace Interact
+{
+    public class ItemInteractable : Interactable
+    {
+        public ItemDataSO item;
+        public string SaveKey => "pick_item_" + item.name;
+        
+        protected override void Start()
+        {
+            base.Start();
+            // 道具已经被拾取了
+            if (SaveManager.GetBool(SaveKey))
+            {
+                gameObject.SetActive(false);
+            }
+        }
+
+        public override void Interact(Interactor interactor)
+        {
+            Debug.Log("与道具交互");
+            UIManager.Instance.ShowPanel<ItemInfoPanel>("ItemInfoPanel", callBack: panel => panel.UpdateInfo(item));
+        }
+    }
+}
