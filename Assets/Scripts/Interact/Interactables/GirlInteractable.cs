a8a952618f4e73436f60350595d4d73936861ff5
Arno Clare <ArnoClareF@outlook.com>
2022-08-11 17:20:46 +0800

    基本完成第三幕 MainBuilding 的场景布置。

---
+++ Assets/Scripts/Interact/Interactables/GirlInteractable.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using Data.Story;
+using Save;
+using Story;
+using UI;
+using UnityEngine;
+
+namespace Interact
+{
+    public class GirlInteractable : StoryInteractable
+    {
+        public PlotDataSO afterPlot;
+
+        public float waitSecond = 2;
+        
+        protected new SpriteRenderer renderer;
+        protected Collider2D coll;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            renderer = GetComponent<SpriteRenderer>();
+            coll = GetComponent<Collider2D>();
+        }
+
+        public override void Interact(Interactor interactor)
+        {
+            StoryManager.Instance.StartStory(plot, FinishPlot);
+            if (triggerOnce)
+            {
+                SaveManager.RegisterBool(SaveKey);
+                DisableSelf();
+            }
+        }
+
+        protected void FinishPlot()
+        {
+            UIManager.Instance.ShowPanel<FaderPanel>("FaderPanel", "", UILayer.Top, panel => {
+                panel.fader.Alpha = 0;
+                StartCoroutine(FaderCoroutine(panel));
+                // 注册遇见女主
+                SaveManager.RegisterBool("MeetGirl");
+            });
+        }
+
+        protected IEnumerator FaderCoroutine(FaderPanel panel)
+        {
+            yield return panel.fader.FadeCoroutine(1);
+
+            renderer.enabled = false;
+            coll.enabled = false;
+            
+            yield return new WaitForSeconds(waitSecond);
+            
+            yield return panel.fader.FadeCoroutine(0);
+            UIManager.Instance.HidePanel("FaderPanel", true);
+            
+            StoryManager.Instance.StartStory(afterPlot);
+        }
+    }
+}
